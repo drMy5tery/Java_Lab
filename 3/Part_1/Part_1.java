23c6812450d214cba1863504a827448e47c38ed9
Jawahar Jeeva <rjeprof@gmail.com>
2024-11-05 12:41:49 +0530

    Final Commit

--- 3/Part_1/Part_1.java
+++ 3/Part_1/Part_1.java
@@ -105,32 +105,9 @@ class ExecutiveEmployee extends SalariedEmployee {
 //main class to test the payroll system
 public class Part_1 {
     public static void main(String[] args) {
-         //create an array of Employee references to store different types of employees
-         Employee[] employees = new Employee[3];
-
-         //instantiate HourlyEmployee
-         employees[0] = new HourlyEmployee(1, "Jawahar", "Teaching Assistant", 20.5, 40);
-         
-         //instantiate SalariedEmployee
-         employees[1] = new SalariedEmployee(2, "Sigma", "All in one", 4000.0);
-         
-         //instantiate ExecutiveEmployee
-         employees[2] = new ExecutiveEmployee(3, "Mikasa", "Head of the Squad", 8000.0, 15.0);
- 
-         //calculate total payroll
-         double totalPayroll = 0;
- 
-         //display detailed information for each employee and calculate total payroll
-         for (Employee employee : employees) {
-             employee.displayInfo();
-             totalPayroll += employee instanceof HourlyEmployee 
-                 ? ((HourlyEmployee) employee).calculateWeeklySalary() 
-                 : ((SalariedEmployee) employee).calculateWeeklySalary();
-             System.out.println();
-         }
- 
-         //display total payroll
-         System.out.println("Total Payroll: " + totalPayroll);
-     }
-    
+        //create and display each employee's information
+        new HourlyEmployee(1, "Jawahar", "Teaching Assistant", 20.5, 40).displayInfo();
+        new SalariedEmployee(2, "Dimple", "Lecturer", 4000).displayInfo();
+        new ExecutiveEmployee(3, "Harsha", "Head of Department", 8000, 15).displayInfo();
+    }
 }
